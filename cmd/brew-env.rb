e7c8ab23378353bd3b771505896b886d9d414617
Shaun Jackman <sjackman@gmail.com>
2019-02-05 14:49:46 -0500

    Symlink bin/brew rather than clone

--- cmd/brew-env.rb
+++ cmd/brew-env.rb
@@ -16,28 +16,29 @@ module Homebrew
     end
 
     case ARGV.named.shift
-    when "new"
+    when "create"
       path = ARGV.named.shift
       odie "must specify a path" if path.nil?
       path = Pathname.new(path)
       ohai "Creating a Homebrew environment in #{path}..."
       mkdir_p path
       chdir path do
-        system "git", "init", "-q"
-        system "git", "config", "remote.origin.url", HOMEBREW_REPOSITORY
-        system "git", "config", "remote.origin.fetch", "+refs/heads/*:refs/remotes/origin/*"
-        system "git", "config", "core.autocrlf", "false"
-        system "git", "fetch", "origin", "master:refs/remotes/origin/master",
-               "--tags", "--force", "--quiet"
-        system "git", "reset", "--quiet", "--hard", "origin/master"
-        system "ln", "-sf", HOMEBREW_CELLAR
-        Dir.chdir "Library" do
-          rmdir "Taps"
-          system "ln", "-sf", HOMEBREW_REPOSITORY/"Library/Taps"
-        end
+        mkdir_p "bin"
+        ln_s HOMEBREW_REPOSITORY/"bin/brew", "bin/brew"
+        ln_s HOMEBREW_CELLAR, "Cellar"
+        fullpath = Pathname.new(Dir.getwd).realpath
+        Pathname.new("bin/brew-activate").write <<~EOS
+          #!/bin/sh
+          eval $(#{fullpath}/bin/brew shellenv)
+        EOS
+        chmod "+x", "bin/brew-activate"
         ohai "Next steps:"
-        puts "- Run `eval $(brew env activate #{path})` to start using your environment"
-        puts "- Run `brew link <formula>` to add an installed formula to your environment"
+        puts <<~EOS
+          - Run `eval $(brew env activate #{path})`
+            or `source #{path}/bin/brew-activate`
+            to start using your environment"
+          - Run `brew link <formula>` to add an installed formula to your environment
+        EOS
       end
     when "activate"
       path = ARGV.named.shift
@@ -45,10 +46,9 @@ module Homebrew
       path = Pathname.new(path)
       odie "must specify a path" unless path.exist?
       odie "must specify a Homebrew environment" unless (path/"bin/brew").exist?
-      realpath = path.realpath
-      puts "export PATH=\"#{realpath}/bin:#{realpath}/sbin:$PATH\""
+      exec path/"bin/brew", "shellenv"
     else
-      odie "subcommand must be one of: new remove activate"
+      odie "subcommand must be one of: create, activate"
     end
   end
 end
